d47a4cbd3e698a3f5ca880801e6213e7fae33621
Colton <kaiserc2@seattleu.edu>
2020-08-25 15:10:09 -0700

    CreateStore, subscribe, and dispatch

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 function buyCake() {
@@ -17,10 +20,20 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
-          ...state,
+        ...state,
         numofCakes: state.numofCakes - 1,
       };
     default:
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
