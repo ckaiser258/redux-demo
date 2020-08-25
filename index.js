4e40ebb1d9d85008e8724b754b647c514b7dcf20
Colton <kaiserc2@seattleu.edu>
2020-08-25 15:22:01 -0700

    Creating multiple reducers

--- index.js
+++ index.js
@@ -19,18 +19,50 @@ function buyIceCream() {
 
 // {previousState, action} => newState
 
-const initialState = {
+// const initialState = {
+//   numofCakes: 10,
+//   numofIceCreams: 20,
+// };
+
+const initialCakeState = {
   numofCakes: 10,
+};
+
+const initialIceCreamState = {
   numofIceCreams: 20,
 };
 
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numofCakes: state.numofCakes - 1,
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         numofIceCreams: state.numofIceCreams - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numofCakes: state.numofCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
