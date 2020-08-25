a22ca2e84ea17ec86a8a21f3ac2087298cc06a38
Colton <kaiserc2@seattleu.edu>
2020-08-25 15:17:37 -0700

    Add second action to one reducer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -10,10 +11,17 @@ function buyCake() {
   };
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
 // {previousState, action} => newState
 
 const initialState = {
   numofCakes: 10,
+  numofIceCreams: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -22,6 +30,11 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numofCakes: state.numofCakes - 1,
+      };
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numofIceCreams: state.numofIceCreams - 1,
       };
     default:
       return state;
@@ -36,4 +49,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
