250bb5e1485070238f9455bfe7609c8344d8ae30
Colton <kaiserc2@seattleu.edu>
2020-08-26 16:02:39 -0700

    Adds applyMiddleware to store

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const reduxLogger = require("redux-logger");
 
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware
 const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = "BUY_CAKE";
@@ -81,11 +82,9 @@ const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("Initial state", store.getState());
-const unsubscribe = store.subscribe(() =>
-  console.log("Updated state", store.getState())
-);
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
