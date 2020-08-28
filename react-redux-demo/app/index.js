abe5bb9ce4d2551e7104ec5732d92ceef9c21ba5
Colton <kaiserc2@seattleu.edu>
2020-08-28 13:51:29 -0700

    Imports Provider and store

--- react-redux-demo/app/index.js
+++ react-redux-demo/app/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CakeContainer from './components/CakeContainer'
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CakeContainer from "./components/CakeContainer";
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
         <div className="App">
-            <CakeContainer />
+          <CakeContainer />
         </div>
-        )
-    }
+      </Provider>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
